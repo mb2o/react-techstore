d03a9aa72bba06ed41f0cbde0110ad9f7dd3c0a6
mb2o <mboomaars@gmail.com>
2019-07-11 10:34:57 +0200

    Google Fonts, CSS Variables and App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <h1 className="text-title">
+      <span className="text-main">Tech</span> Store
+    </h1>
   );
 }
 
