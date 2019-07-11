ebb373f0cf293eb0d30e6b20cb127e7aa7f00070
mb2o <mboomaars@gmail.com>
2019-07-12 00:30:18 +0200

    Formspree

--- src/components/ContactPage/Contact.js
+++ src/components/ContactPage/Contact.js
@@ -7,7 +7,10 @@ export default function Contact() {
       <div className="row">
         <div className="col-10 mx-auto col-md-6 my-3">
           <Title title="contact us" center />
-          <form className="mt-5">
+          <form
+            className="mt-5"
+            action="https://formspree.io/mboomaars@gmail.com"
+            method="POST">
             <div className="form-group">
               <input
                 type="text"
@@ -37,6 +40,14 @@ export default function Contact() {
 
             <div className="form-group">
               <textarea name="message" className="form-control" rows="10" />
+            </div>
+
+            <div className="form-group mt-3">
+              <input
+                type="submit"
+                value="Send"
+                className="form-control bg-primary text-white"
+              />
             </div>
           </form>
         </div>
