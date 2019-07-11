953ffe9da8df103af9d18cc95690207c0c26a6c8
mb2o <mboomaars@gmail.com>
2019-07-12 01:49:55 +0200

    Featured Products Section

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../../context';
+import Product from '../Product';
+import Title from '../Title';
 
 export default function Featured() {
-  return <div>Hello from Featured</div>;
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="featured products" center />
+        <div className="row">
+          <ProductConsumer>
+            {({ featuredProducts }) => {
+              return featuredProducts.map(product => (
+                <Product key={product.id} product={product} />
+              ));
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </section>
+  );
 }
