2b90a746a73d3b25813d15f174f07ddb772b55c5
mb2o <mboomaars@gmail.com>
2019-07-13 14:07:23 +0200

    Cart Item Component

--- src/components/CartPage/CartItem.js
+++ src/components/CartPage/CartItem.js
@@ -1,5 +1,65 @@
 import React from 'react';
+import {
+  FaChevronCircleDown,
+  FaChevronCircleUp,
+  FaTrash
+} from 'react-icons/fa';
 
-export default function CartItem() {
-  return <div>CartItem</div>;
+export default function CartItem({
+  cartItem,
+  increment,
+  decrement,
+  removeItemFromCart
+}) {
+  const { id, title, price, count, total, image } = cartItem;
+
+  return (
+    <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <img
+          src={image}
+          alt="cartitem image"
+          width="60px"
+          className="image-fluid"
+        />
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">product : </span>
+        {title}
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">price : </span>${price}
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <FaChevronCircleDown
+              className="cart-icon text-primary"
+              onClick={() => decrement(id)}
+            />
+            <span className="text-title text-muted mx-3">{count}</span>
+            <FaChevronCircleUp
+              className="cart-icon text-primary"
+              onClick={() => increment(id)}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <FaTrash
+          className="text-danger cart-icon"
+          onClick={() => removeItemFromCart(id)}
+        />
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none">total : </span>
+        <strong>${total}</strong>
+      </div>
+    </div>
+  );
 }
