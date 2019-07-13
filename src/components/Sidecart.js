fc4e455bd039279dbf97931c85e6d5f088d12cd9
mb2o <mboomaars@gmail.com>
2019-07-13 12:44:28 +0200

    SideCart Component

--- src/components/Sidecart.js
+++ src/components/Sidecart.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
 
 export default function Sidecart() {
   return (
     <ProductConsumer>
-      {({ cartOpen, closeCart, cart }) => {
+      {({ cartOpen, closeCart, cart, cartTotal }) => {
         return (
           <SidecartWrapper show={cartOpen} onClick={closeCart}>
-            <p>cart items</p>
+            <ul>
+              {cart.map(item => (
+                <li key={item.id} className="cart-item mb-4">
+                  <img src={`../${item.image}`} width="35px" alt="cart item" />
+                  <div className="mt-3">
+                    <h6 className="text-uppercase">{item.title}</h6>
+                    <h6 className="text-title text-capitalize">
+                      amount : {item.count}
+                    </h6>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <h4 className="text-capitalize text-main">
+              cart total : ${cartTotal}
+            </h4>
+            <div className="text-center my-5">
+              <Link to="/cart" className="main-link">
+                cart page
+              </Link>
+            </div>
           </SidecartWrapper>
         );
       }}
@@ -27,6 +48,16 @@ const SidecartWrapper = styled.div`
   border-left: 4px solid var(--primaryColor);
   transition: var(--mainTransition);
   transform: ${props => (props.show ? 'translateX(0)' : 'translateX(100%)')};
+  overflow: scroll;
+  padding: 2rem;
+
+  ul {
+    padding: 0 !important;
+  }
+
+  .cart-item {
+    list-style-type: none;
+  }
 
   @media (min-width: 576px) {
     width: 20rem;
